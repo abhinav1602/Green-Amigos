dd9572c3f2e567c937fd338608cb05f5c8f4424d
Abhishek Tiwari <tiwari.abhishek428@gmail.com>
2017-06-27 01:33:04 +0530

    added killer,arrow,liberty and sasta mart

--- js/clientsController.js
+++ js/clientsController.js
@@ -1,37 +1,84 @@
-angular.module('myApp',[]).controller('clientsCtrl',function($scope){
-	$scope.clients =[
-		{   location :'Raymonds, Civil Lines, Allahabad',
-			msg : "Congratulations to Raymond's civil lines Allahabad branch on being the first of many on joining the revolutionary wave of Green Amigos. Their valiant contributions in our humble efforts to save the environment are truly appreciated and something to be proud of.",
-			img : 'raymond', 
-			key1 : 'Duplex : ',
-			key2 : 'Old corrugated containers : ',
-			value1 : '24.29 kg',
-			value2 : '79.47 kg'
-		},
-
-		{ location : 'Maharaja Footwear KP Kakkar Road,South Malaka ,Allahabad' , 
-		  msg :"Great Job Maharaja Footwear, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
-		  img : 'maharaja',
-		  key1 : 'Recyclable Material : ',
-		  key2 : '',
-		  value1 : '150 kg',
-		  value2 : ''
-		},
-		{ location : 'Anant Bazaar,Naini ,Allahabad' , 
-		  msg :"Great Job Anant Bazaar, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
-		  img : 'anant_bazaar',
-		  key1 : 'Recyclable Material : ',
-		  key2 : '',
-		  value1 : '22 kg',
-		  value2 : ''
-		},
-		{ location : 'Chaddha electric works' , 
-		  msg :"Great Job Chaddha electric works, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
-		  img : 'Chaddha_electric_works',
-		  key1 : 'Recyclable Material : ',
-		  key2 : '',
-		  value1 : '91 kg',
-		  value2 : ''
-		}
-	];
+angular.module('myApp', []).controller('clientsCtrl', function($scope) {
+    $scope.clients = [{
+            location: 'Raymonds, Civil Lines, Allahabad',
+            msg: "Congratulations to Raymond's civil lines Allahabad branch on being the first of many on joining the revolutionary wave of Green Amigos. Their valiant contributions in our humble efforts to save the environment are truly appreciated and something to be proud of.",
+            img: 'raymond',
+            key1: 'Duplex : ',
+            key2: 'Old corrugated containers : ',
+            key3: 'Cartons : ',
+            value1: '33.705 kg',
+            value2: '79.47 kg',
+            value3: '26.85 kg'
+        }, {
+            location: 'Killer, Civil Lines, Allahabad',
+            msg: "Great Job Killer, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
+            img: 'killer',
+            key1: 'Cartons : ',
+            key2: '',
+            key3: '',
+            value1: '7 kg',
+            value2: '',
+            value3: ''
+        },  {
+            location: 'Arrow, Civil Lines, Allahabad',
+            msg: "Great Arrow, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
+            img: 'arrow',
+            key1: 'Cartons : ',
+            key2: '',
+            key3: '',
+            value1: '16.67 kg',
+            value2: '',
+            value3: ''
+        }, {
+            location: 'Linerty, Civil Lines, Allahabad',
+            msg: "Great Job Liberty, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
+            img: 'liberty',
+            key1: 'Cartons : ',
+            key2: '',
+            key3: '',
+            value1: '30 kg',
+            value2: '',
+            value3: ''
+        }, {
+            location: 'Maharaja Footwear KP Kakkar Road,South Malaka ,Allahabad',
+            msg: "Great Job Maharaja Footwear, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
+            img: 'maharaja',
+            key1: 'Recyclable Material : ',
+            key2: '',
+            key3: '',
+            value1: '150 kg',
+            value2: '',
+            value3: ''
+        }, {
+            location: 'Sasta Mart, Civil Lines, Allahabad',
+            msg: "Great Job Sasta Mart, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
+            img: 'sasta_mart',
+            key1: 'Duplex : ',
+            key2: 'Cartons : ',
+            key3: 'Polythene : ',
+            value1: '13.54 kg',
+            value2: '37 kg',
+            value3: '13.35 kg'
+        }, {
+            location: 'Anant Bazaar,Naini ,Allahabad',
+            msg: "Great Job Anant Bazaar, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
+            img: 'anant_bazaar',
+            key1: 'Recyclable Material : ',
+            key2: '',
+            key3: '',
+            value1: '22 kg',
+            value2: '',
+            value3: ''
+        }, {
+            location: 'Chaddha electric works',
+            msg: "Great Job Chaddha electric works, Allahabad! We thank you for joining us in our stand to protect the environment. The city is proud of you.",
+            img: 'Chaddha_electric_works',
+            key1: 'Recyclable Material : ',
+            key2: '',
+            key3: '',
+            value1: '91 kg',
+            value2: '',
+            value3: ''
+        }
+    ];
 });
